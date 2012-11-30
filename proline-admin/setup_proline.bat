58d06de2a3ae244fd02d85ec343bb2fefcccc147
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-11-30 15:20:57 +0000

    Minor fix

--- proline-admin/setup_proline.bat
+++ proline-admin/setup_proline.bat
@@ -1,2 +1,3 @@
-java -cp "target/lib/*;target/Proline-Admin-0.0.1-SNAPSHOT.jar" fr.proline.admin.RunCommand
+cd target
+java -cp "lib/*;Proline-Admin-0.0.1-SNAPSHOT.jar" fr.proline.admin.RunCommand setup
 pause
