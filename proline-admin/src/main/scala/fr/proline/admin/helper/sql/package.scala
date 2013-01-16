359fba1ef5eed30ef6573f426fc4e5fa6a0dea5a
Christophe Bruley <christophe.bruley@cea.fr>
2013-01-16 16:20:30 +0000

    - bug correction : Pg needs a valid connection in order to create a new database. Now uses a connection to template1 to create db before running migration

--- proline-admin/src/main/scala/fr/proline/admin/helper/sql/package.scala
+++ proline-admin/src/main/scala/fr/proline/admin/helper/sql/package.scala
@@ -1,16 +1,19 @@
 package fr.proline.admin.helper
 
-import javax.persistence.{EntityManagerFactory,Persistence}
+import javax.persistence.{ EntityManagerFactory, Persistence }
 import com.weiglewilczek.slf4s.Logger
-
 import fr.proline.repository.IDatabaseConnector
+import org.postgresql.util.PSQLException
+import java.sql.Connection
+import java.sql.DriverManager
+import fr.proline.admin.service.db.setup.DatabaseSetupConfig
 
 /**
  * @author David Bouyssie
  *
  */
 package object sql {
-  
+
   /*def buildEmfUsingPgWorkaround( connector: DatabaseConnector,
                                  db: ProlineRepository.Databases ): EntityManagerFactory = {
     
@@ -24,33 +27,55 @@ package object sql {
       JPAUtil.PersistenceUnitNames.getPersistenceUnitNameForDB(db), emSettings
     )
   }*/
-  
-  def createPgDatabase( pgDbConnector: IDatabaseConnector, dbName: String, logger: Option[Logger] = None ) {
+
+  def createPgDatabase(dbConfig: DatabaseSetupConfig, logger: Option[Logger] = None) {
+
+    val pgDbConnector = dbConfig.connector
     
     // Create database connection and statement
-    val pgDbConn = pgDbConnector.getDataSource.getConnection
+    val pgDbConn = {
+      try {
+        pgDbConnector.getDataSource.getConnection
+      } catch {
+        case psqle: PSQLException =>
+          {
+            classOf[org.postgresql.Driver]
+            val templateURL = "jdbc:postgresql://"+dbConfig.connectionConfig.getString("host")+":"++dbConfig.connectionConfig.getString("port")+"/template1"
+            if (logger != None) logger.get.info("creating database from template '%s'...".format(templateURL))
+
+            val pgTemplateConn = DriverManager.getConnection(templateURL, dbConfig.connectionConfig.getString("user"), dbConfig.connectionConfig.getString("password"))
+            val stmt = pgTemplateConn.createStatement
+            // Create database if it doesn't exists
+            if (_checkDbExists(stmt, dbConfig.dbName) == false) {
+              if (logger != None) logger.get.info("creating database '%s'...".format(dbConfig.dbName))
+              stmt.executeUpdate("CREATE DATABASE %s;".format(dbConfig.dbName))
+            }
+
+            // Close database connection and statement
+            stmt.close()
+            pgTemplateConn.close()
+            pgDbConnector.getDataSource.getConnection
+          }
+      } 
+      }.asInstanceOf[Connection]
+    
+
     val stmt = pgDbConn.createStatement
     
-    // Create database if it doesn't exists
-    if( _checkDbExists(stmt,dbName) == false ) {
-      if( logger != None ) logger.get.info("creating database '%s'...".format(dbName))
-      stmt.executeUpdate("CREATE DATABASE %s;".format(dbName) )
-    }
-    
     // Check that database has been created
-    if( _checkDbExists(stmt,dbName) == false )
-      throw new Exception( "can't create database '%s'".format(dbName) )
-    
+    if (_checkDbExists(stmt, dbConfig.dbName) == false)
+      throw new Exception("can't create database '%s'".format(dbConfig.dbName))
+
     // Close database connection and statement
     stmt.close()
-    pgDbConn.close()   
+    pgDbConn.close()
   }
-  
+
   private def _checkDbExists(stmt: java.sql.Statement, dbName: String): Boolean = {
     val jdbcRS = stmt.executeQuery("select count(*) from pg_catalog.pg_database where datname = '%s'"
-                     .format(dbName))
-    
-    if( jdbcRS.next() && jdbcRS.getInt(1) == 0 ) false else true
+      .format(dbName))
+
+    if (jdbcRS.next() && jdbcRS.getInt(1) == 0) false else true
   }
 
 }
