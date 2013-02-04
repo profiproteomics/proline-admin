159e144a88fbc3367bf29a28a7d322fcbc558f84
Christophe Bruley <christophe.bruley@cea.fr>
2013-02-04 14:55:28 +0000

    - simplified command for proline. TODO : This files must be relocated in a resource folder and packaged properly by maven

---
+++ proline-admin/runCmd.bat
@@ -0,0 +1,2 @@
+java -cp "target/lib/*;target/Proline-Admin-0.0.4-SNAPSHOT.jar;target/config" -Dlogback.configurationFile=target/config/logback.xml fr.proline.admin.RunCommand %*
+pause
--- proline-admin/setup_proline.bat
+++ proline-admin/setup_proline.bat
@@ -1,2 +1,2 @@
-java -cp "target/lib/*;target/Proline-Admin-0.0.3-SNAPSHOT.jar;target/config" -Dlogback.configurationFile=target/config/logback.xml fr.proline.admin.RunCommand setup
+java -Xmx1024m -cp "target/lib/*;target/Proline-Admin-0.0.4-SNAPSHOT.jar;target/config" -Dlogback.configurationFile=target/config/logback.xml fr.proline.admin.RunCommand setup
 pause
