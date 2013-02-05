d4dae697e9efc1ad70a3f6484cc4500c804f975a
Christophe Bruley <christophe.bruley@cea.fr>
2013-02-05 16:12:02 +0000

    moved in scripts folder

--- proline-admin/runCmd.bat
+++
@@ -1,2 +0,0 @@
-java -cp "target/lib/*;target/Proline-Admin-0.0.4-SNAPSHOT.jar;target/config" -Dlogback.configurationFile=target/config/logback.xml fr.proline.admin.RunCommand %*
-pause
--- proline-admin/setup_proline.bat
+++
@@ -1,2 +0,0 @@
-java -Xmx1024m -cp "target/lib/*;target/Proline-Admin-0.0.4-SNAPSHOT.jar;target/config" -Dlogback.configurationFile=target/config/logback.xml fr.proline.admin.RunCommand setup
-pause
