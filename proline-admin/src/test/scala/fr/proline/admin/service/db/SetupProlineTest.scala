1ed8d69a0cdbef75440618bd9838e9489f7a741f
Laurent Martin <laurent.martin2@cea.fr>
2013-02-06 14:51:30 +0000

    Put the @Rule JUnit annotation on an instance field instead of Singleton / static one.

--- proline-admin/src/test/scala/fr/proline/admin/service/db/SetupProlineTest.scala
+++ proline-admin/src/test/scala/fr/proline/admin/service/db/SetupProlineTest.scala
@@ -8,33 +8,28 @@ import org.junit.rules.TemporaryFolder
 import com.typesafe.config.ConfigFactory
 import com.weiglewilczek.slf4s.Logging
 
-object SetupProlineTest {
-  @Rule
-  def tmpDirectory: TemporaryFolder = new TemporaryFolder()
-}
-
 class SetupProlineTest extends Logging {
-    
+
+  @Rule // JUnit Rule annotation doesn't like static fields
+  var tmpDirectory: TemporaryFolder = new TemporaryFolder()
+
   @Test
   def testProlineSetup() {
-    
-    val dataTmpDir = SetupProlineTest.tmpDirectory.newFolder("proline_data_tmp_dir").getAbsolutePath()
-    
+    val dataTmpDir = tmpDirectory.newFolder("proline_data_tmp_dir").getAbsolutePath()
+
     val inMemoryConfig = ConfigFactory.parseMap(
       Map(
         "proline-config.driver-type" -> "h2",
         "proline-config.data-directory" -> dataTmpDir,
-        "h2-config.connection-properties.connectionMode" -> "MEMORY"
-      )
-    )
-    
+        "h2-config.connection-properties.connectionMode" -> "MEMORY"))
+
     // Load application config and replace some properties values with the previous ones
-    var classLoader = SetupProline.getClass().getClassLoader()  
-    val appConf = inMemoryConfig.withFallback( ConfigFactory.load(classLoader,"application") ) 
-    
+    var classLoader = SetupProline.getClass().getClassLoader()
+    val appConf = inMemoryConfig.withFallback(ConfigFactory.load(classLoader, "application"))
+
     val prolineSetupConfig = SetupProline.parseProlineSetupConfig(appConf)
-    
-    new SetupProline( prolineSetupConfig ).run()
+
+    new SetupProline(prolineSetupConfig).run()
   }
 
 }
