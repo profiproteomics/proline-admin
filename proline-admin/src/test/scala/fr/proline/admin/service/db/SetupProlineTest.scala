b5d64177f0c282aabc147e453114b0c3a7418102
Laurent Martin <laurent.martin2@cea.fr>
2013-02-06 15:39:09 +0000

    Manually create a temp directory instead of usige @Rule JUnit annotation.

--- proline-admin/src/test/scala/fr/proline/admin/service/db/SetupProlineTest.scala
+++ proline-admin/src/test/scala/fr/proline/admin/service/db/SetupProlineTest.scala
@@ -7,15 +7,30 @@ import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import com.typesafe.config.ConfigFactory
 import com.weiglewilczek.slf4s.Logging
+import org.junit.Before
+import java.util.Date
 
 class SetupProlineTest extends Logging {
 
-  @Rule // JUnit Rule annotation doesn't like static fields
-  var tmpDirectory: TemporaryFolder = new TemporaryFolder()
+  /* JUnit @Rule annotation does not work in Scala : 
+   * http://stackoverflow.com/questions/7352087/how-can-i-use-junit-expectedexception-in-scala
+   */
+  var tmpDir: File = null
+
+  @Before
+  def createTmpDir() {
+    val now = new Date()
+    val tmpDirName = String.format("temp_%tY%<tm%<td%<tH%<tM%<tS_%<tL", now)
+
+    tmpDir = new File(tmpDirName)
+    tmpDir.mkdir()
+
+    tmpDir.deleteOnExit()
+  }
 
   @Test
   def testProlineSetup() {
-    val dataTmpDir = tmpDirectory.newFolder("proline_data_tmp_dir").getAbsolutePath()
+    val dataTmpDir = tmpDir.getAbsolutePath
 
     val inMemoryConfig = ConfigFactory.parseMap(
       Map(
