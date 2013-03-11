102ba5cf3e47d9bfb540937d220ebef9277ab61b
Laurent Martin <laurent.martin2@cea.fr>
2013-03-11 10:52:11 +0000

    Replace some @println@ by loggers.
    Improve exception handling with logger in catch blocks.

--- proline-admin/src/main/scala/fr/proline/admin/RunCommand.scala
+++ proline-admin/src/main/scala/fr/proline/admin/RunCommand.scala
@@ -2,61 +2,64 @@ package fr.proline.admin
 
 import java.io.File
 import com.weiglewilczek.slf4s.Logging
-import com.beust.jcommander.{JCommander,MissingCommandException,Parameter,ParameterException,Parameters}
+import com.beust.jcommander.{ JCommander, MissingCommandException, Parameter, ParameterException, Parameters }
 import collection.JavaConversions._
 import fr.proline.admin.service.db.SetupProline
-import fr.proline.admin.service.db.{DatabaseConnectionContext,ProlineDatabaseContext}
+import fr.proline.admin.service.db.{ DatabaseConnectionContext, ProlineDatabaseContext }
+import fr.proline.util.ThreadLogger
 
 object RunCommand extends App with Logging {
-  
+
   trait JCommandReflection {
-    lazy private val _parametersAnnotation = this.getClass().getAnnotation( classOf[Parameters] )
-    
+    lazy private val _parametersAnnotation = this.getClass().getAnnotation(classOf[Parameters])
+
     object Parameters {
       lazy val names = _parametersAnnotation.commandNames()
       lazy val firstName = names(0)
       lazy val description = _parametersAnnotation.commandDescription()
     }
   }
-  
-  @Parameters(commandNames = Array("setup"), commandDescription = "Set Up the Proline databases", separators = "=" )
+
+  @Parameters(commandNames = Array("setup"), commandDescription = "Set Up the Proline databases", separators = "=")
   private object SetupProlineCommand extends JCommandReflection
-  
-  @Parameters(commandNames = Array("create_project"), commandDescription = "Create new project", separators = "=" )
+
+  @Parameters(commandNames = Array("create_project"), commandDescription = "Create new project", separators = "=")
   private object CreateProjectCommand extends JCommandReflection {
-    
-    @Parameter(names = Array("--owner_id","-oid"), description = "The user account id of the project owner", required = true )
+
+    @Parameter(names = Array("--owner_id", "-oid"), description = "The user account id of the project owner", required = true)
     var ownerId: Int = 0
-    
-    @Parameter(names = Array("--name","-n"), description = "The project name", required = true )
+
+    @Parameter(names = Array("--name", "-n"), description = "The project name", required = true)
     var projectName: String = ""
-    
-    @Parameter(names = Array("--description","-desc"), description = "The project description", required = false )
+
+    @Parameter(names = Array("--description", "-desc"), description = "The project description", required = false)
     var projectDescription: String = ""
   }
-  
-  @Parameters(commandNames = Array("create_user"), commandDescription = "Create new user account", separators = "=" )
-  private object CreateUserCommand extends JCommandReflection {    
-    @Parameter(names = Array("--login","-l"), description = "The user account login", required = true )
+
+  @Parameters(commandNames = Array("create_user"), commandDescription = "Create new user account", separators = "=")
+  private object CreateUserCommand extends JCommandReflection {
+    @Parameter(names = Array("--login", "-l"), description = "The user account login", required = true)
     var userLogin: String = ""
   }
-  
+
   override def main(args: Array[String]): Unit = {
-    
+
+    Thread.currentThread.setUncaughtExceptionHandler(new ThreadLogger("fr.proline.admin.RunCommand"))
+
     // Instantiate a JCommander object and affect some commands
-    val jCmd = new JCommander()  
-    jCmd.addCommand( SetupProlineCommand )
-    jCmd.addCommand( CreateProjectCommand )
-    jCmd.addCommand( CreateUserCommand )
-    
+    val jCmd = new JCommander()
+    jCmd.addCommand(SetupProlineCommand)
+    jCmd.addCommand(CreateProjectCommand)
+    jCmd.addCommand(CreateUserCommand)
+
     // Try to parse the command line
     var parsedCommand = ""
     try {
       jCmd.parse(args: _*)
-      
+
       parsedCommand = jCmd.getParsedCommand()
-      println("Running '"+ parsedCommand +"' command...")
-      
+      println("Running '" + parsedCommand + "' command...")
+
       // Execute parsed command
       parsedCommand match {
         case SetupProlineCommand.Parameters.firstName => {
@@ -64,43 +67,44 @@ object RunCommand extends App with Logging {
           SetupProline()
         }
         case CreateProjectCommand.Parameters.firstName => {
-          
+
           import fr.proline.admin.service.user.CreateProject
-          
+
           val projectId = CreateProject(
             CreateProjectCommand.projectName,
             CreateProjectCommand.projectDescription,
             CreateProjectCommand.ownerId
           )
-          
-          this.logger.info("project with id='"+ projectId +"' has been created !")
-          
+
+          this.logger.info("project with id='" + projectId + "' has been created !")
+
         }
-        case CreateUserCommand.Parameters.firstName => {          
+        case CreateUserCommand.Parameters.firstName => {
           import fr.proline.admin.service.user.CreateUser
-          CreateUser( CreateUserCommand.userLogin )
+          CreateUser(CreateUserCommand.userLogin)
         }
         case _ => {
-          throw new MissingCommandException("unknown command '"+ jCmd.getParsedCommand()+"'" )
+          throw new MissingCommandException("unknown command '" + jCmd.getParsedCommand() + "'")
         }
       }
-      
+
     } catch {
-      case pe: ParameterException => {
-        println("Invalid command or parameter:")
-        println(pe.getMessage)
+
+      case pEx: ParameterException => {
+        println()
+        logger.warn("Invalid command or parameter", pEx)
         jCmd.usage()
       }
-      case e: Throwable => {
-        println("Execution of command '"+parsedCommand+"' failed because:")
-        println(e.getMessage+"\n")
-        println("Error stack trace:")
-        println(e.getStackTraceString)
+
+      case ex: Exception => {
+        println()
+        logger.error("Execution of command '" + parsedCommand + "' failed", ex)
       }
+
     }
-    
+
   }
-  
+
   /*def createProject( projectName: String,
                      projectDescription: String,
                      ownerId: Int ) {
--- proline-admin/src/main/scala/fr/proline/admin/service/db/setup/ISetupDB.scala
+++ proline-admin/src/main/scala/fr/proline/admin/service/db/setup/ISetupDB.scala
@@ -1,65 +1,73 @@
 package fr.proline.admin.service.db.setup
 
-import java.io.{File,InputStream}
+import java.io.{ File, InputStream }
 import scala.io.Source
 import com.googlecode.flyway.core.Flyway
 import com.weiglewilczek.slf4s.Logging
-
 import fr.proline.admin.helper.sql._
-import fr.proline.repository.{IDatabaseConnector,DatabaseUpgrader,DriverType}
+import fr.proline.repository.{ IDatabaseConnector, DatabaseUpgrader, DriverType }
 import fr.proline.util.io._
 import fr.proline.util.resources._
+import fr.proline.util.ThreadLogger
 
 /**
  * @author David Bouyssie
  *
  */
-trait ISetupDB extends Logging {  
-  
+trait ISetupDB extends Logging {
+
   val dbConfig: DatabaseSetupConfig
   private var _executed = false
-  
+
   // Interface
   protected def importDefaults(): Unit
-  
-   /** Execution state. */
+
+  /** Execution state. */
   def isExecuted = this._executed
-  
+
   /** Execute the setup of the database. */
   def run() {
-    if( _executed )
+    if (_executed)
       throw new IllegalStateException("the setup has been already executed")
-    
+
+    val currentThread = Thread.currentThread
+
+    if (!currentThread.isInstanceOf[ThreadLogger]) {
+      currentThread.setUncaughtExceptionHandler(new ThreadLogger("fr.proline.admin.service.db.setup.ISetupDB"))
+    }
+
     try {
-      if( this.initSchema() ) {
+      if (this.initSchema()) {
         this.importDefaults()
-        this.logger.info("database '"+dbConfig.dbName +"' successfully set up !")
+        logger.info("database '" + dbConfig.dbName + "' successfully set up !")
       }
     } catch {
-      case e: Throwable => {
-        this.logger.error(dbConfig.dbName + " schema initialization failed", e)
+
+      case ex: Exception => {
+        logger.error(dbConfig.dbName + " schema initialization failed", ex)
       }
+
     }
-    
+
     this._executed = true
   }
-  
+
   protected def initSchema(): Boolean = {
-    
+
     val dbConnector = dbConfig.connector
-    
+
     // Create database if driver type is PostgreSQL
-    if( dbConfig.driverType == DriverType.POSTGRESQL ) {
-      createPgDatabase( dbConfig, Some(this.logger) )
+    if (dbConfig.driverType == DriverType.POSTGRESQL) {
+      createPgDatabase(dbConfig, Some(this.logger))
     }
-    
+
     // Initialize database schema
-//    dbConfig.schemaVersion = DatabaseUpgrader.upgradeDatabase(dbConnector);    
-//    if ((dbConfig.schemaVersion == null) || (dbConfig.schemaVersion.isEmpty()) || dbConfig.schemaVersion.equals("no.version")) false else true
+    //    dbConfig.schemaVersion = DatabaseUpgrader.upgradeDatabase(dbConnector);    
+    //    if ((dbConfig.schemaVersion == null) || (dbConfig.schemaVersion.isEmpty()) || dbConfig.schemaVersion.equals("no.version")) false else true
     dbConfig.schemaVersion = "0.1"
-    if( DatabaseUpgrader.upgradeDatabase(dbConnector) > 0 ) true else false
-    
-   /*    
+    if (DatabaseUpgrader.upgradeDatabase(dbConnector) > 0) true else false
+
+    /*    
     // If driver type is SQLite (flyway doesn't support SQLite at the moment)
     if( dbConfig.driverType == DriverType.SQLITE ) {
       
@@ -89,7 +97,7 @@ trait ISetupDB extends Logging {
     }*/
 
   }
-  
+
   /*protected def createSQLiteDB( connector: IDatabaseConnector, scriptIS: InputStream ): Boolean = {
     
     // If connection mode is file
@@ -120,5 +128,5 @@ trait ISetupDB extends Logging {
     createDB
           
   }*/
-  
+
 }
