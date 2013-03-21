10e1af91b910136cde3ee5ce272dc8a3d1f1f2f9
Laurent Martin <laurent.martin2@cea.fr>
2013-03-21 09:56:26 +0000

    Improve traces of DatabaseConnector creation / close for Server context use.

--- proline-admin/src/main/scala/fr/proline/admin/service/db/setup/ProlineSetupConfig.scala
+++ proline-admin/src/main/scala/fr/proline/admin/service/db/setup/ProlineSetupConfig.scala
@@ -2,131 +2,137 @@ package fr.proline.admin.service.db.setup
 
 import java.io.File
 import scala.collection.JavaConversions.collectionAsScalaIterable
-import com.typesafe.config.{Config,ConfigFactory,ConfigList}
-import fr.proline.core.orm.uds.{ExternalDb => UdsExternalDb,
-                                ExternalDbPropertiesSerializer => UdsExtDbPropsSerializer }
+import com.typesafe.config.{ Config, ConfigFactory, ConfigList }
+import fr.proline.core.orm.uds.{
+  ExternalDb => UdsExternalDb,
+  ExternalDbPropertiesSerializer => UdsExtDbPropsSerializer
+}
 import fr.proline.repository._
+import com.weiglewilczek.slf4s.Logging
 
 /** Configuration settings for Proline setup */
 case class ProlineSetupConfig(
-             dataDirectory: File,
-             udsDBConfig: DatabaseSetupConfig,
-             udsDBDefaults: UdsDBDefaults,
-             pdiDBConfig: DatabaseSetupConfig,
-             pdiDBDefaults: PdiDBDefaults,
-             psDBConfig: DatabaseSetupConfig,
-             msiDBConfig: DatabaseSetupConfig,
-             msiDBDefaults: MsiDBDefaults,
-             lcmsDBConfig: DatabaseSetupConfig
-             ) {
-  
+  dataDirectory: File,
+  udsDBConfig: DatabaseSetupConfig,
+  udsDBDefaults: UdsDBDefaults,
+  pdiDBConfig: DatabaseSetupConfig,
+  pdiDBDefaults: PdiDBDefaults,
+  psDBConfig: DatabaseSetupConfig,
+  msiDBConfig: DatabaseSetupConfig,
+  msiDBDefaults: MsiDBDefaults,
+  lcmsDBConfig: DatabaseSetupConfig) {
+
   // Check that directory exists
-  require( dataDirectory.exists() && dataDirectory.isDirectory() )
-  
+  require(dataDirectory.exists() && dataDirectory.isDirectory())
+
 }
 
 object DatabaseSetupConfig {
-  private val connectionProperties = new java.util.HashMap[String,String]()
-  connectionProperties.put("dbName","")
-  connectionProperties.put("connectionMode","")
-  connectionProperties.put("user","")
-  connectionProperties.put("password","")
-  connectionProperties.put("host","")
-  connectionProperties.put("port","")
-  connectionProperties.put("driver","")
+  private val connectionProperties = new java.util.HashMap[String, String]()
+  connectionProperties.put("dbName", "")
+  connectionProperties.put("connectionMode", "")
+  connectionProperties.put("user", "")
+  connectionProperties.put("password", "")
+  connectionProperties.put("host", "")
+  connectionProperties.put("port", "")
+  connectionProperties.put("driver", "")
   //connectionProperties.put("hibernate.dialect","")
-  
+
   val connectionConfigSchema = ConfigFactory.parseMap(connectionProperties)
 }
 
 /** Configuration settings for database setup */
-case class DatabaseSetupConfig( dbType: ProlineDatabaseType,
-                                driverType: DriverType,
-                                dbDirectory: File,
-                                connectionConfig: Config
-                                ) {
-  
+case class DatabaseSetupConfig(dbType: ProlineDatabaseType,
+                               driverType: DriverType,
+                               dbDirectory: File,
+                               connectionConfig: Config) extends Logging {
+
   // Check that directories exists
   //require( scriptDirectory.exists() && scriptDirectory.isDirectory(), "missing script directory:"+scriptDirectory )
-  require( dbDirectory.exists() && dbDirectory.isDirectory(), "missing database directory:"+dbDirectory )
-  
+  require(dbDirectory.exists() && dbDirectory.isDirectory(), "missing database directory:" + dbDirectory)
+
   // Check configuration validity
   connectionConfig.checkValid(DatabaseSetupConfig.connectionConfigSchema)
-  
+
   var dbName = connectionConfig.getString("dbName")
-  var connectionMode = ConnectionMode.valueOf( connectionConfig.getString("connectionMode") )
+  var connectionMode = ConnectionMode.valueOf(connectionConfig.getString("connectionMode"))
   var schemaVersion = "no.version"
-  
-  lazy val dbConnProperties = {    
+
+  lazy val dbConnProperties = {
     this.toUdsExternalDb.toPropertiesMap()
   }
-  
+
   /*lazy val dbConnPrototype = {
     new ConnectionPrototype(dbConnProperties).namePrefix("")
   }*/
-  
+
+  // FIXME This Admin Connector is never closed : conflicting with DataStoreConnectorFactory
   lazy val connector = this.toNewConnector
-  
+
   private def toNewConnector() = {
     // Instantiate the database connector
     //val db = ProlineRepository.Databases.valueOf(dbType.toUpperCase)
     //dbConnPrototype.toConnector( dbName )
-    
-    val dbConnProps = dbConnProperties.asInstanceOf[java.util.Map[Object,Object]]
-    DatabaseConnectorFactory.createDatabaseConnectorInstance( dbType, dbConnProps)
+
+    val dbConnProps = dbConnProperties.asInstanceOf[java.util.Map[Object, Object]]
+
+    val databaseConnector = DatabaseConnectorFactory.createDatabaseConnectorInstance(dbType, dbConnProps)
+
+    val ex = new Exception("Raw DatabaseConnector creation")
+
+    logger.warn("Creation of raw DatabaseConnector [" + databaseConnector + "] without DataStoreConnectorFactory", ex)
+
+    databaseConnector
   }
-  
+
   def toUdsExternalDb(): UdsExternalDb = {
-    
+
     //val dbConnProps = this.dbConnProperties
-    
+
     val udsExtDb = new UdsExternalDb()
-    if( connectionMode == ConnectionMode.FILE ) udsExtDb.setDbName( dbDirectory + "/" + dbName )
-    else udsExtDb.setDbName( dbName )
-    
-    udsExtDb.setType( this.dbType )
-    udsExtDb.setDbVersion( this.schemaVersion )
-    udsExtDb.setIsBusy( false )
+    if (connectionMode == ConnectionMode.FILE) udsExtDb.setDbName(dbDirectory + "/" + dbName)
+    else udsExtDb.setDbName(dbName)
+
+    udsExtDb.setType(this.dbType)
+    udsExtDb.setDbVersion(this.schemaVersion)
+    udsExtDb.setIsBusy(false)
     udsExtDb.setConnectionMode(connectionMode)
-    
+
     val userName = connectionConfig.getString("user")
-    if( userName.length > 0 ) udsExtDb.setDbUser(userName)
-    else if( driverType != DriverType.SQLITE ) {
+    if (userName.length > 0) udsExtDb.setDbUser(userName)
+    else if (driverType != DriverType.SQLITE) {
       throw new Exception("a user name is required in database authentification configuration")
     }
-    
+
     val password = connectionConfig.getString("password")
-    if( password.length > 0 ) udsExtDb.setDbPassword(password)
-    
+    if (password.length > 0) udsExtDb.setDbPassword(password)
+
     val host = connectionConfig.getString("host")
-    if( host.length > 0 ) udsExtDb.setHost(host)
-    
+    if (host.length > 0) udsExtDb.setHost(host)
+
     val port = connectionConfig.getString("port")
-    if( port.length > 0 ) udsExtDb.setPort(port.toInt)
-    
+    if (port.length > 0) udsExtDb.setPort(port.toInt)
+
     udsExtDb.setDriverType(driverType)
-    
+
     // Serialize properties
     UdsExtDbPropsSerializer.serialize(udsExtDb)
-    
+
     udsExtDb
   }
-  
+
 }
 
 case class MsiDBDefaults(
-             scorings: java.util.List[Config],
-             schemata: java.util.List[Config]
-            )
-            
+  scorings: java.util.List[Config],
+  schemata: java.util.List[Config])
+
 case class PdiDBDefaults(
-             resources: Config
-            )
-            
+  resources: Config)
+
 case class UdsDBDefaults(
-             resources: Config,
-             instruments: java.util.List[Config],             
-             peaklistSoftware: java.util.List[Config],
-             quantMethods: java.util.List[Config]
-            )
+  resources: Config,
+  instruments: java.util.List[Config],
+  peaklistSoftware: java.util.List[Config],
+  quantMethods: java.util.List[Config])
             
--- proline-admin/src/main/scala/fr/proline/admin/service/user/CreateProject.scala
+++ proline-admin/src/main/scala/fr/proline/admin/service/user/CreateProject.scala
@@ -2,117 +2,126 @@ package fr.proline.admin.service.user
 
 import com.weiglewilczek.slf4s.Logging
 
-import fr.profi.jdbc.easy._
-import fr.proline.admin.service.db.SetupProline
-import fr.proline.admin.service.db.DatabaseConnectionContext
-import fr.proline.core.orm.uds.{Project => UdsProject,UserAccount => UdsUser}
-import fr.proline.core.orm.util.DataStoreConnectorFactory
+import fr.profi.jdbc.easy.{ date2Formattable, int2Formattable, string2Formattable }
+import fr.proline.admin.service.db.{ CreateProjectDBs, DatabaseConnectionContext, SetupProline }
 
 /**
  * @author David Bouyssie
  *
  */
-class CreateProject( udsDbContext: DatabaseConnectionContext,
-                     projectName: String,
-                     projectDescription: String,
-                     ownerId: Int ) extends Logging {
-  
+class CreateProject(udsDbContext: DatabaseConnectionContext,
+                    projectName: String,
+                    projectDescription: String,
+                    ownerId: Int) extends Logging {
+
   var projectId = 0
-  
+
   def run() {
-    
+
     // Get EasyDBC object    
     val udsEzDBC = udsDbContext.ezDBC
-    
+
     // Check connection and transaction
     val wasConnOpened = udsDbContext.isConnectionOpened()
     val wasInTx = udsEzDBC.isInTransaction()
-    
-    if( !wasInTx ) udsEzDBC.beginTransaction()
-    
-    this.projectId = udsEzDBC.executePrepared("INSERT INTO project (name,description,creation_timestamp,owner_id) VALUES (?,?,?,?)",true) { stmt =>
-      stmt.executeWith( projectName,
-                        projectDescription,
-                        new java.util.Date(),
-                        ownerId
-                       )
+
+    if (!wasInTx) udsEzDBC.beginTransaction()
+
+    this.projectId = udsEzDBC.executePrepared("INSERT INTO project (name,description,creation_timestamp,owner_id) VALUES (?,?,?,?)", true) { stmt =>
+      stmt.executeWith(projectName,
+        projectDescription,
+        new java.util.Date(),
+        ownerId
+      )
       //this.projectId = udsDb.extractGeneratedInt( stmt.wrapped )
       stmt.generatedInt
     }
-    
+
     // Manage connection and transaction
-    if( !wasInTx ) udsEzDBC.commitTransaction()
-    if( !wasConnOpened ) udsDbContext.closeConnection()
+    if (!wasInTx) udsEzDBC.commitTransaction()
+    if (!wasConnOpened) udsDbContext.closeConnection()
 
     // import fr.proline.core.orm.uds.{Project => UdsProject,UserAccount => UdsUser}
     // Creation UDS entity manager
     //val udsEM = dbManager.udsEMF.createEntityManager()
-    
+
     // Retrieve the owner => we assume it has been already created
     //val query = udsEM.createQuery("Select user from UserAccount user where id = :id", classOf[UdsUser])
     //query.setParameter("id", CreateProjectCommand.ownerId )
-    
+
     //val owner = query.getSingleResult
     //println("creating project for user with login '"+ owner.getLogin +"'...")
-    
+
     // Create the project
     //val udsProject = new UdsProject( owner )
     //udsProject.setName( CreateProjectCommand.projectName )
     //udsProject.setDescription( CreateProjectCommand.projectDescription )          
     //udsProject.addMember( owner )
-    
+
     //udsEM.persist( udsProject )
-    
+
     //val projectId = udsProject.getId
     //println("project with id='"+ projectId +"' has been created !")
-    
+
     // Close entity manager
     //udsEM.close()
   }
-  
+
 }
 
-object CreateProject {
-  
-  def apply( name: String, description: String, ownerId: Int ): Int = {
-    
-    import fr.proline.admin.service.db.{CreateProjectDBs,DatabaseConnectionContext,ProlineDatabaseContext}
-    
+object CreateProject extends Logging {
+
+  def apply(name: String, description: String, ownerId: Int): Int = {
+
+    import fr.proline.admin.service.db.{ CreateProjectDBs, DatabaseConnectionContext, ProlineDatabaseContext }
+
     // Retrieve Proline configuration
     val prolineConf = SetupProline.config
-    
+
     // Instantiate a database manager
     val udsDBConfig = prolineConf.udsDBConfig
     val udsDbConnector = udsDBConfig.connector
     val udsDbContext = new DatabaseConnectionContext(udsDbConnector)
-    
-    // Create project
-    val projectCreator = new CreateProject(
-      udsDbContext,
-      name,
-      description,
-      ownerId
-    )
-    projectCreator.run()
-    
-    // Close the database resources
-    //udsDbContext.closeAll()
-    
-    // Create a new database manager to avoid any conflict
-    //val dsConnectorFactory = DataStoreConnectorFactory.getInstance()
-    //if( dsConnectorFactory.isInitialized == false ) dsConnectorFactory.initialize(udsDbConnector)
-    
-    //val prolineDbContext = new ProlineDatabaseContext(dsConnectorFactory)
-    
-    // Create project databases
-    new CreateProjectDBs( udsDbContext, prolineConf, projectCreator.projectId ).run()          
-    
-    // Close the database manager
-    udsDbContext.closeAll()
-    
+
+    var projectCreator: CreateProject = null
+
+    try {
+
+      // Create project
+      projectCreator = new CreateProject(
+        udsDbContext,
+        name,
+        description,
+        ownerId
+      )
+      projectCreator.run()
+
+      // Close the database resources
+      //udsDbContext.closeAll()
+
+      // Create a new database manager to avoid any conflict
+      //val dsConnectorFactory = DataStoreConnectorFactory.getInstance()
+      //if( dsConnectorFactory.isInitialized == false ) dsConnectorFactory.initialize(udsDbConnector)
+
+      //val prolineDbContext = new ProlineDatabaseContext(dsConnectorFactory)
+
+      // Create project databases
+      new CreateProjectDBs(udsDbContext, prolineConf, projectCreator.projectId).run()
+
+      // Close the database manager
+
+    } finally {
+      logger.debug("Closing current UDS Db Context")
+
+      try {
+        udsDbContext.closeAll()
+      } catch {
+        case exClose: Exception => logger.error("Error closing UDS Db Context", exClose)
+      }
+
+    }
+
     projectCreator.projectId
-    
   }
 
-  
 }
