7cb64f6d9a480f374f9fa050aee75b3ff39c9531
Julie Poisat <julie.poisat@ipbs.fr>
2014-09-16 11:23:24 +0000

    fix : wait for user's answer about creating new data dir or not before updating Proline config

--- proline-admin-gui/src/main/scala/fr/proline/admin/gui/process/ProlineAdminConnection.scala
+++ proline-admin-gui/src/main/scala/fr/proline/admin/gui/process/ProlineAdminConnection.scala
@@ -1,46 +1,78 @@
 package fr.proline.admin.gui.process
 
 import java.io.File
+
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.slf4j.Logging
+
 import fr.proline.admin.gui.Main
-import fr.proline.admin.gui.Util
+import fr.proline.admin.gui.component.dialog.GetConfirmation
 import fr.proline.admin.gui.component.panel.ButtonsPanel
 import fr.proline.admin.service.db.SetupProline
+
 import scalafx.application.Platform
-import scalafx.beans.property.BooleanProperty.sfxBooleanProperty2jfx
-import fr.proline.admin.gui.component.dialog.ConfirmationDialog
-import fr.proline.admin.gui.component.dialog.GetConfirmation
+import scalafx.scene.Cursor
+import scalafx.scene.Cursor.sfxCursor2jfx
 
 /**
  * All utilities to modify ProlineAdmin configuration
  */
-object ProlineAdminConnection {
-  //TODO: make it a class?
+object ProlineAdminConnection extends Logging {
 
-  def updateProlineConf() = LaunchAction(
+  /**
+   * Udapte Proline Admin config (processing + UI management)
+   */
+  def updateProlineConf() = {
+    //BLOCKING! otherwise update config before (conf file changes || user's choice) (are||is) effective
+    //FIXME: freezing
 
-    actionButton = ButtonsPanel.editConfButton,
-    actionString = "<b>>> Updating Proline configuration...</b>",
-    action = () => {
+    val actionString = "<b>>> Updating Proline configuration...</b>"
+
+    //    LaunchAction(
+    //      actionButton = ButtonsPanel.editConfButton, //Array[Button] ?
+    //      actionString = actionString,
+    //      action = () => {
+
+    synchronized {
+      Main.stage.scene().setCursor(Cursor.WAIT)
+      println()
+      println(actionString)
 
       try {
+
         this._setNewProlineConfig()
-        //        ButtonsPanel.updateBooleans()
+        ButtonsPanel.computeButtonsAvailability()
+
+        //        logger.info(s"Action '$actionString' finished with success.")
+        logger.debug(s"Action '$actionString' finished with success.")
+
+        println(s"""[ $actionString : <b>success</b> ]""")
 
       } catch {
         case e: Exception => {
+          ButtonsPanel.disableAllButEdit()
 
-          Platform.runLater {
-            ButtonsPanel.disableAllButEdit()
-            //TODO: logger here?
-            // println("  ERROR - Could not set new Proline configuration : " + e)
-            // e.printStackTrace() //TODO: add all stack traces to console log
-          }
-          throw new Exception("Could noooot update proline configuration")
+//          logger.error("LOGGER ERROR Can't update Proline configuration") //TODO: or throw custom exception?
+          logger.debug("LOGGER DEBUG Can't update Proline configuration") //TODO: or throw custom exception?
+          logger.debug(e.getMessage()) //stackTrace?
+
+          //          Platform.runLater {
+          println("ERROR - Can't update Proline configuration : " + e.getMessage())
+          println(s"[ $actionString : finished with <b>error</b> ]")
+          //          }
+
+          //throw e // if re-thrown, system stops
         }
+
+      } finally {
+        //        ButtonsPanel.computeButtonsAvailability()
+        Main.stage.scene().setCursor(Cursor.DEFAULT)
       }
+
     }
-  )
+    //    )
+
+  }
 
   /**
    *  Update SetupProline config when CONF file changes
@@ -50,46 +82,123 @@ object ProlineAdminConnection {
     /** Reload CONF file */
     val newConfigFile = ConfigFactory.parseFile(new File(Main.confPath))
 
-    /** Update displayed window, and Proline configuration if data directory exists */
+    /** Update displayed window, and Proline configuration if data directory already exists */
     val dataDir = newConfigFile.getConfig("proline-config").getString("data-directory")
 
     if (new File(dataDir).exists()) {
-      println("dataDir exists") //TODO: delete me
 
-      println("already exists, set conf")
+      //      synchronized {
       SetupProline.setConfigParams(newConfigFile)
+      UdsRepository.setUdsDbConfig(SetupProline.getUpdatedConfig.udsDBConfig)
+
       Platform.runLater(Main.stage.title = s"Proline Admin @ $dataDir")
+      //      }
 
-      /** Allow to create data folder if it doesn't exist */
-    } else {
-      println("dataDir doesn't exist")
+    } /** Allow to create data folder if it doesn't exist */ else {
 
-      Platform.runLater {
-        val isConfirmed =
-          GetConfirmation(
-            "The databases directory you specified does not exist. Do you want to create it?\n(This involves the creation of a new installation of Proline.)",
-            s"Unknown directory : $dataDir"
-          )
-        if (isConfirmed == true) {
-          val successfullyCreated = new File(dataDir).mkdir()
+//      logger.warn(s"LOGGER WARN Unknown data directory : $dataDir")
+      logger.debug(s"LOGGER DEBUG Unknown data directory : $dataDir")
+      println(s"WARN - Unknown data directory : $dataDir")
 
-          if (successfullyCreated == true) {
-            println("Databases directory successfully created.")
-            println("confirmed, set conf")
-            SetupProline.setConfigParams(newConfigFile)
-            Platform.runLater(Main.stage.title = s"Proline Admin @ $dataDir")
+      val isConfirmed = GetConfirmation(
+        text = "The databases directory you specified does not exist. Do you want to create it?\n(This involves the creation of a new installation of Proline.)",
+        title = s"Unknown directory : $dataDir"
+      )
 
-            /** If it can't be created */
-          } else {
-            Platform.runLater(Main.stage.title = s"Proline Admin (invalid configuration)")
-            throw new Exception("Unknown data directory " + dataDir)
-          }
+      //      Platform.runLater {
+      if (isConfirmed == true) {
+        //        logger.info(s"Creating data directory : $dataDir")
+        logger.debug(s"Creating data directory : $dataDir")
+        println(s"Creating databases directory : $dataDir ...")
+        val successfullyCreated = new File(dataDir).mkdir()
 
+        if (successfullyCreated == true) {
+          logger.info("Data directory successfully created.")
+          println("INFO - Databases directory successfully created.")
+
+          SetupProline.setConfigParams(newConfigFile)
+          UdsRepository.setUdsDbConfig(SetupProline.getUpdatedConfig.udsDBConfig)
+
+          Platform.runLater(Main.stage.title = s"Proline Admin @ $dataDir")
+
+          /** If it can't be created */
         } else {
           Platform.runLater(Main.stage.title = s"Proline Admin (invalid configuration)")
-          throw new Exception("Unknown data directory " + dataDir)
+          throw new Exception("Unknown data directory (problem in creation) " + dataDir)
         }
+
+      } else {
+        Platform.runLater(Main.stage.title = s"Proline Admin (invalid configuration)")
+        throw new Exception("Unknown data directory (not created, user's choice) " + dataDir)
       }
+      //      }
+
+      ///////////////////////// TRY ////////////////////////
+      // FROM http://docs.scala-lang.org/overviews/core/futures.html
+      //
+      //      import scala.concurrent._
+      //      import scala.concurrent.ExecutionContext.Implicits.global
+      //
+      //      val p = promise[Boolean]
+      //      val f = p.future
+      //
+      //      val producer =
+      //        future 
+      //        {
+      //          println("in promise")
+      //          val isConfirmed = Platform.runLater(
+      //            GetConfirmation(
+      //              text = "The databases directory you specified does not exist. Do you want to create it?\n(This involves the creation of a new installation of Proline.)",
+      //              title = s"Unknown directory : $dataDir"
+      //            ))
+      //          p success isConfirmed
+      //          continueDoingSomethingUnrelated()
+      //        }
+      //
+      //      val consumer = future {
+      //
+      //      logger.warn(s"Unknown data directory : $dataDir")
+      //      println(s"WARN - Unknown data directory : $dataDir")
+      //
+      //      f onFailure { case e => logger.error(e.getMessage()); println("ERROR - " + e.getMessage()) }
+      //      f onSuccess {
+      //        case isConfirmed => {
+      //
+      //          println("WARN - isConfirmed : " + isConfirmed)
+      //
+      //          if (isConfirmed == true) {
+      //            logger.info(s"Creating data directory : $dataDir")
+      //            println(s"Creating databases directory : $dataDir ...")
+      //
+      //            val successfullyCreated = new File(dataDir).mkdir()
+      //
+      //            /** If it's created */
+      //            if (successfullyCreated == true) {
+      //              logger.info("Data directory successfully created.")
+      //              println("Databases directory successfully created.")
+      //
+      //              SetupProline.setConfigParams(newConfigFile)
+      //              UdsRepository.setUdsDbConfig(SetupProline.getUpdatedConfig.udsDBConfig)
+      //
+      //              Platform.runLater(Main.stage.title = s"Proline Admin @ $dataDir")
+      //
+      //              /** If it can't be created */
+      //            } else {
+      //              Platform.runLater(Main.stage.title = s"Proline Admin (invalid configuration)")
+      //              throw new Exception("Unknown data directory (problem in creation) " + dataDir)
+      //            }
+      //
+      //            /** If user doesn't want to create it */
+      //          } else {
+      //            Platform.runLater(Main.stage.title = s"Proline Admin (invalid configuration)")
+      //            throw new Exception("Unknown data directory (not created, user's choice) " + dataDir)
+      //          }
+      //
+      //        }
+      //        }
+      //      }
+      ///////////////// END ////////////////////
+
     }
   }
 }
