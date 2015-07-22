368f7e9c3f843d675ef26d81671423f1b0d904ce
Julie Poisat <julie.poisat@ipbs.fr>
2015-07-22 10:18:46 +0000

    Add enumeration for image resources

---
+++ proline-admin-gui/src/main/scala/fr/proline/admin/gui/IconResource.scala
@@ -0,0 +1,8 @@
+package fr.proline.admin.gui
+
+object IconResource extends Enumeration {
+
+  val EDIT = Value("/images/edit.png")
+  val HELP = Value("/images/help.png")
+  val WARNING = Value("/images/warning.png")
+}
