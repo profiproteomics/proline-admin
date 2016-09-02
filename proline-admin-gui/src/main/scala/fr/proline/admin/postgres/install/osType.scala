650e551cfbe64167699e876b7717738830dc5bcd
romdhani1987 <romdhani@live.fr>
2016-09-02 09:40:38 +0000

    determine if postgres is installed already (to do)

---
+++ proline-admin-gui/src/main/scala/fr/proline/admin/postgres/install/osType.scala
@@ -0,0 +1,41 @@
+package fr.proline.admin.postgres.install
+import com.typesafe.scalalogging.LazyLogging
+import scala.collection.mutable.ArrayBuffer
+
+
+
+/**
+ * check version of os and check if postgresq is installed  
+ **/
+object CheckInstalledPostgres {
+  
+  private  var  OS:String=System.getProperty("os.name").toLowerCase()
+  private  var command:String=_
+  
+  /* check postgre and showmeaaage  alert if not installed  */
+  
+   def checkPostgres():Unit={
+   
+  }
+  
+  /* get the operating system:Windows or unix */
+  
+  def isWindows():Boolean={
+    return (OS.indexOf("win") >= 0)
+  }
+  def isMac():Boolean={
+    return (OS.indexOf("mac") >= 0)
+  }
+  def isUnix():Boolean={
+    return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 )
+  }
+  def isSolaris():Boolean={
+    return (OS.indexOf("sunos") >= 0);
+  }
+ /* warning message */
+  
+ def showPopUp():Unit={
+   
+ }
+
+}
