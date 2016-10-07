cf0ea4415bae30299dcfb46423de998bc600ccdd
romdhani1987 <romdhani@live.fr>
2016-10-07 13:30:26 +0000

    make a div scrollable.

---
+++ proline-admin-gui/src/main/scala/fr/proline/admin/gui/component/configuration/form/AbstractMountFilesWizard.scala
@@ -0,0 +1,23 @@
+package fr.proline.admin.gui.component.configuration.form
+import scalafx.Includes._
+import scalafx.geometry.Insets
+import scalafx.geometry.Pos
+import scalafx.scene.control.Button
+import scalafx.scene.control.ScrollPane.ScrollBarPolicy
+import scalafx.scene.control.Tab
+import scalafx.scene.layout.HBox
+import scalafx.scene.layout.VBox
+import scalafx.stage.Stage
+import fr.profi.util.scalafx.CustomScrollPane
+import fr.profi.util.scalafx.TitledBorderPane
+import scalafx.beans.property.DoubleProperty
+import scalafx.scene.Node
+
+abstract class AbstractMountFilesWizard extends CustomScrollPane(hBarPolicy = ScrollBarPolicy.AS_NEEDED) {
+
+
+  /* Initialize form to null */
+  maxHeight=570
+    
+  
+}
---
+++ proline-admin-gui/src/main/scala/fr/proline/admin/gui/component/configuration/form/MountFilesContent.scala
@@ -0,0 +1,26 @@
+package fr.proline.admin.gui.component.configuration.form
+
+import fr.proline.admin.gui.component.configuration.form._
+import scalafx.Includes._
+import scalafx.geometry.Insets
+import scalafx.geometry.Pos
+import scalafx.scene.layout.VBox
+import scalafx.stage.Stage
+import fr.proline.admin.gui.QuickStart
+import fr.proline.admin.gui.component.configuration.file._
+class MountFilesContent extends AbstractMountFilesWizard  {
+ 
+ var mountfiles=new MonutFiles()
+ setContentNode(
+    new VBox {
+      alignmentInParent = Pos.TOP_RIGHT
+      spacing = 5
+      minWidth = 730
+      maxWidth = 730
+      content = List(
+        mountfiles
+      )
+    }
+  )
+  
+}
