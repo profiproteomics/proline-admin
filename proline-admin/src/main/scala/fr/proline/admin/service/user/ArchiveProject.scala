66f39ccb2567b3491a9054fd76a8511f1ea69cf1
romdhani1987 <romdhani@live.fr>
2016-12-09 13:36:24 +0000

    use csvPrinter to store data .

--- proline-admin/src/main/scala/fr/proline/admin/service/user/ArchiveProject.scala
+++ proline-admin/src/main/scala/fr/proline/admin/service/user/ArchiveProject.scala
@@ -20,9 +20,14 @@ import java.util.Date
 import java.text.SimpleDateFormat
 import scala.sys.process.Process
 import scala.sys.process.ProcessLogger
+import scala.collection.JavaConversions._
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
+
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import org.apache.commons.csv.CSVRecord
 
 /**
  *  pg_dump  :
@@ -51,7 +56,7 @@ class ArchiveProject(dsConnectorFactory: IDataStoreConnectorFactory, projectId: 
         if (project != null) {
           val externalDbMsi = ExternalDbRepository.findExternalByTypeAndProject(udsEM, fr.proline.repository.ProlineDatabaseType.MSI, project)
           val externalDbLcms = ExternalDbRepository.findExternalByTypeAndProject(udsEM, fr.proline.repository.ProlineDatabaseType.LCMS, project)
-          val createPgDumpSelectedRowsTables = new StringBuilder
+
           // get the properties of the project to update
           val properties = project.getSerializedProperties()
           var parser = new JsonParser()
@@ -67,71 +72,65 @@ class ArchiveProject(dsConnectorFactory: IDataStoreConnectorFactory, projectId: 
             (externalDbMsi.getDbName() != null) && (!externalDbMsi.getDbName().isEmpty) && (externalDbLcms.getDbName() != null) && (!externalDbLcms.getDbName().isEmpty) && (externalDbMsi.getPort() > 0)) {
             try {
               logger.info(s"Starting to backup the project #$projectId....")
+
               //pgdump  databases for a project 
               logger.info(s"Starting to pg_dump of MSI,LCMS and schema of UDS databases...")
               if (execPgDump(externalDbMsi.getHost(), externalDbMsi.getPort(), externalDbMsi.getDbUser(), externalDbMsi.getDbPassword(), externalDbMsi.getDbName(), externalDbLcms.getDbName(), pathDestination, pathSource, projectId)) {
-                //Select some rows from uds_db only for the current project :
-                //rows project 
-                createPgDumpSelectedRowsTables.append("project").append("").append("\n")
-                createPgDumpSelectedRowsTables.append(projectId).append("|").append(project.getName()).append("|").append(project.getDescription()).append("|").append(project.getCreationTimestamp()).append("|").
-                  append(project.getSerializedProperties()).append("|").append(project.getOwner().getId()).append("|").append(project.getLockExpirationTimestamp()).append("|").append(project.getLockUserID()).append("").append("\n")
-                //rows of externaldb(msi)
-                createPgDumpSelectedRowsTables.append("External_db").append("").append("\n")
-                createPgDumpSelectedRowsTables.append(externalDbMsi.getId()).append("|").append(externalDbMsi.getDbName()).append("|").append(externalDbMsi.getConnectionMode()).append("|").append(externalDbMsi.getDbUser())
-                  .append("|").append(externalDbMsi.getDbPassword()).append("|").append(externalDbMsi.getHost()).append("|").append(externalDbMsi.getPort()).append("|").append(externalDbMsi.getType()).append("|")
-                  .append(externalDbMsi.getDbVersion()).append("|").append(externalDbMsi.getIsBusy()).append("|").append(externalDbMsi.getSerializedProperties()).append("").append("\n")
-                // rows of externaldb(lcms)
-                createPgDumpSelectedRowsTables.append(externalDbLcms.getId()).append("|").append(externalDbLcms.getDbName()).append("|").append(externalDbLcms.getConnectionMode()).append("|").append(externalDbLcms.getDbUser())
-                  .append("|").append(externalDbLcms.getDbPassword()).append("|").append(externalDbLcms.getHost()).append("|").append(externalDbLcms.getPort()).append("|").append(externalDbLcms.getType()).append("|")
-                  .append(externalDbLcms.getDbVersion()).append("|").append(externalDbLcms.getIsBusy()).append("|").append(externalDbLcms.getSerializedProperties()).append("").append("\n")
+                val fileToWrite = new File(pathDestination, "\\project_" + projectId + "\\uds_db_data.csv")
 
+                var csvPrinter: CSVPrinter = null
+
+                csvPrinter = new CSVPrinter(new FileWriter(fileToWrite.getAbsoluteFile()), CSVFormat.MYSQL.withDelimiter('#'));
+
+                //rows project 
+
+                csvPrinter.printRecord(projectId.toString, project.getName(), project.getDescription(), project.getCreationTimestamp(), project.getSerializedProperties(), project.getOwner().getId().toString,
+                  project.getLockExpirationTimestamp(), project.getLockUserID());
+
+                //rows of externaldb(msi)
+
+                csvPrinter.printRecord(externalDbMsi.getId().toString, externalDbMsi.getDbName(), externalDbMsi.getConnectionMode(), externalDbMsi.getDbUser(), externalDbMsi.getDbPassword(),
+                  externalDbMsi.getHost(), externalDbMsi.getPort(), externalDbMsi.getType(), externalDbMsi.getDbVersion(), externalDbMsi.getIsBusy().toString, externalDbMsi.getSerializedProperties())
+
+                // rows of externaldb(lcms)
+
+                csvPrinter.printRecord(externalDbLcms.getId().toString, externalDbLcms.getDbName(), externalDbLcms.getConnectionMode(), externalDbLcms.getDbUser(), externalDbLcms.getDbPassword(),
+                  externalDbLcms.getHost(), externalDbLcms.getPort(), externalDbLcms.getType(), externalDbLcms.getDbVersion(), externalDbLcms.getIsBusy().toString, externalDbLcms.getSerializedProperties())
                 DoJDBCWork.withEzDBC(udsDbCtx) { ezDBC =>
                   // rows of project_db_map
-                  createPgDumpSelectedRowsTables.append("project_db_map").append("").append("\n")
+
                   ezDBC.selectAndProcess(" SELECT project_id,external_db_id FROM project_db_map WHERE project_id=" + projectId) { record =>
                     if (record.getLong("project_id") > 0 && record.getLong("external_db_id") > 0) {
-                      createPgDumpSelectedRowsTables.append(record.getLong("project_id")).append("|").append(record.getLong("external_db_id")).append("|")
-                        .append("").append("\n")
+
+                      csvPrinter.printRecord(record.getLong("project_id").toString, record.getLong("external_db_id").toString)
                     }
                   }
                   //rows of dataset
-                  createPgDumpSelectedRowsTables.append("data_set").append("").append("\n")
+                  csvPrinter.printRecord("dataset")
                   ezDBC.selectAndProcess(" SELECT id,number,name,description ,type ,keywords ,creation_timestamp ,modification_log,children_count ,serialized_properties, result_set_id ,result_summary_id ,aggregation_id ,fractionation_id ,quant_method_id ,parent_dataset_id ,project_id FROM data_set WHERE project_id=" + projectId) { record =>
                     if (record.getLong("id") > 0 && record.getInt("number") >= 0 && record.getString("name") != null && record.getString("type") != null && record.getInt("children_count") >= 0 && record.getLong("project_id") > 0) {
-                      createPgDumpSelectedRowsTables.append(record.getLong("id")).append("|").append(record.getInt("number")).append("|").append(record.getString("name")).append("|").append(record.getString("description"))
-                        .append("|").append(record.getString("type")).append("|").append(record.getString("keywords")).append("|").append(record.getString("creation_timestamp"))
-                        .append("|").append(record.getString("modification_log")).append("|").append(record.getInt("children_count")).append("|").append(record.getStringOption("serialized_properties")).append("|").append(record.getLong("result_set_id"))
-                        .append("|").append(record.getLong("result_summary_id")).append("|").append(record.getLong("aggregation_id")).append("|").append(record.getLong("fractionation_id")).append("|").append(record.getLong("quant_method_id")).append("|")
-                        .append(record.getLong("parent_dataset_id")).append("|").append(record.getLong("project_id"))
-                        .append("").append("\n")
+                      csvPrinter.printRecord(record.getLong("id").toString, record.getInt("number").toString, record.getString("name"), record.getString("description"), record.getString("type"), record.getString("keywords"),
+                        record.getString("creation_timestamp"), record.getString("modification_log"), record.getInt("children_count").toString, record.getStringOption("serialized_properties"), record.getLong("result_set_id").toString,
+                        record.getLong("result_summary_id").toString, record.getLong("aggregation_id").toString, record.getLong("fractionation_id").toString, record.getLong("quant_method_id").toString, record.getLong("parent_dataset_id").toString,
+                        record.getLong("project_id").toString)
                     }
                   }
                   //rows of run_identification
-                  createPgDumpSelectedRowsTables.append("run_identification").append("").append("\n")
+                  csvPrinter.printRecord("runidentification")
+
                   ezDBC.selectAndProcess(" SELECT id,serialized_properties,run_id,raw_file_identifier FROM run_identification WHERE id in (SELECT id FROM data_set WHERE project_id=" + projectId + ")") { record =>
                     if (record.getLong("id") > 0) {
-                      createPgDumpSelectedRowsTables.append(record.getLong("id")).append("|").append(record.getString("serialized_properties")).append("|").append(record.getLong("run_id")).append("|").append(record.getString("raw_file_identifier"))
-                        .append("").append("\n")
+                      csvPrinter.printRecord(record.getLong("id").toString, record.getString("serialized_properties"), record.getLong("run_id").toString, record.getString("raw_file_identifier"))
                     }
                   }
                 }
-                try {
-                  val fileToWrite = new File(pathDestination, "\\\\pg_dump_project_" + projectId + "\\uds_db_project_" + projectId + ".csv")
-                  val fw = new FileWriter(fileToWrite.getAbsoluteFile())
-                  val bw = new BufferedWriter(fw)
-                  bw.write(createPgDumpSelectedRowsTables.toString())
-                  bw.flush()
-                  bw.close()
-                } catch {
-                  case ex: IOException => logger.error("IOException", ex)
-                }
+
+                csvPrinter.flush()
+                csvPrinter.close()
+
               }
             } catch {
               case t: Throwable => logger.error("Error while archiving project", t)
-            } finally {
-              //empty stringbuilder 
-              if ((createPgDumpSelectedRowsTables != null) && (createPgDumpSelectedRowsTables.length > 0))
-                createPgDumpSelectedRowsTables.setLength(0)
             }
             //update serialized properties 
             if (!array.has("archived")) {
@@ -178,7 +177,7 @@ class ArchiveProject(dsConnectorFactory: IDataStoreConnectorFactory, projectId: 
 
   def execPgDump(host: String, port: Integer, user: String, passWord: String, msiDb: String, lcmsDb: String, pathDestination: String, pathSource: String, projectId: Long): Boolean = {
 
-    val pathDestinationProject = new File(pathDestination, "\\pg_dump_project_" + projectId)
+    val pathDestinationProject = new File(pathDestination, "\\project_" + projectId)
     val pathSrcDump = new File(pathSource, "\\pg_dump").getCanonicalPath()
     var pgDumpIsOk: Boolean = false
     try {
@@ -213,10 +212,10 @@ class ArchiveProject(dsConnectorFactory: IDataStoreConnectorFactory, projectId: 
           execute(cmd)
           pgDumpIsOk = true
         } else {
-          logger.error("failed trying to create the directory pg_dump_project_" + projectId + "")
+          logger.error("failed trying to create the directory project_" + projectId + "")
         }
       } else {
-        logger.error("the directory pg_dump_project_" + projectId + " already exist !")
+        logger.error("the directory project_" + projectId + " already exist !")
       }
     } catch {
       case e: Exception => logger.error("error to execute cmd", e)
