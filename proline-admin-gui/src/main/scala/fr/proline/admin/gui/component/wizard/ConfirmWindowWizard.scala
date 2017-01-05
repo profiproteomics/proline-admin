3e828bfc9efa97aa126d0699497d1d2c9cfa785c
romdhani1987 <romdhani@live.fr>
2017-01-05 10:03:21 +0000

    add confirm dialog to exit

---
+++ proline-admin-gui/src/main/scala/fr/proline/admin/gui/component/wizard/ConfirmWindowWizard.scala
@@ -0,0 +1,96 @@
+package fr.proline.admin.gui.component.wizard
+
+import scalafx.Includes._
+import scalafx.geometry.Insets
+import scalafx.geometry.Pos
+import scalafx.scene.Scene
+import scalafx.scene.control.Button
+import scalafx.scene.control.Label
+import scalafx.scene.input.KeyEvent
+import scalafx.scene.layout.StackPane
+import scalafx.stage.Modality
+import scalafx.stage.Stage
+import fr.profi.util.scalafx.ScalaFxUtils
+import fr.proline.admin.gui.QuickStart
+import scalafx.scene.layout.VBox
+import scalafx.scene.layout.HBox
+
+/**
+ * **************************************** *
+ * Show a confirmWindow with given properties *
+ * **************************************** *
+ */
+class ConfirmWindowWizard(
+
+  wTitle: String,
+  wText: String,
+  wParent: Option[Stage] = Option(QuickStart.stage),
+  isResizable: Boolean = false) extends Stage {
+
+  //TODO: rename package into window, for this is no dialog
+  // TODO: see scalafx.stage.PopupWindow
+
+  val popup = this
+
+  title = wTitle
+  initModality(Modality.WINDOW_MODAL)
+  resizable = isResizable
+  if (wParent.isDefined) initOwner(wParent.get)
+
+  //height = 200
+  //      if (X.isDefined) this.x = X.get
+  //      if (Y.isDefined) this.y = Y.get
+
+  scene = new Scene {
+
+    onKeyPressed = (ke: KeyEvent) => { ScalaFxUtils.closeIfEscapePressed(popup, ke) }
+
+    root = new VBox {
+      alignment = Pos.Center
+      spacing = 25
+      padding = Insets(10)
+      //content = new TextArea {
+      content = List(
+
+        new Label(wText) {
+          wrapText = true
+          //editable = false
+          //style = "-fx-border-style: none;-fx-focus-color: transparent;"
+        },
+        new HBox {
+          spacing = 175
+          content = Seq(
+            new Button("cancel") {
+              onAction = handle { popup.close() }
+            },
+            new Button("OK") {
+              onAction = handle { QuickStart.stage.close() }
+            })
+
+        } //        new Button("cancel") {
+        //          onAction = handle { popup.close() }
+        //        },
+        //          new Button("OK") {
+        //          onAction = handle { QuickStart.stage.close() }
+        //        }
+        )
+    }
+  }
+}
+
+/**
+ * ********************************************************************** *
+ * Companion object to cConfirmWindow, show it with more understandable name *
+ * ********************************************************************** *
+ */
+object ShowConfirmWindow {
+
+  def apply(
+    wText: String,
+    wTitle: String = "",
+    wParent: Option[Stage] = Option(QuickStart.stage),
+    isResizable: Boolean = false) {
+    new ConfirmWindowWizard(wTitle, wText, wParent, isResizable).showAndWait()
+  }
+
+}
