b77b4b49df2daf21f9ccafb2b02d9be63f246886
romdhani1987 <romdhani@live.fr>
2017-03-23 12:30:01 +0000

    [proline-Admin] : adapt Tableview

--- proline-admin-gui/src/main/scala/fr/proline/admin/gui/component/resource/implicits/package.scala
+++ proline-admin-gui/src/main/scala/fr/proline/admin/gui/component/resource/implicits/package.scala
@@ -3,7 +3,11 @@ package fr.proline.admin.gui.component.resource
 import scalafx.beans.property.ObjectProperty
 import fr.proline.core.orm.uds.Project
 import fr.proline.core.orm.uds.UserAccount
-
+import fr.proline.core.orm.uds.ExternalDb
+import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
+import fr.proline.repository.ProlineDatabaseType
+import fr.proline.admin.gui.process.UdsRepository
 
 package object implicits {
 
@@ -14,13 +18,27 @@ package object implicits {
    */
   implicit class ProjectView(udsProject: Project) {
     //class ProjectView(owner:UserAccount, project: Project, schemaVersion: String, dbSize:Double) {
-
+  
     val id = new ObjectProperty(this, "id", udsProject.getId)
     val ownerLogin = new ObjectProperty(this, "owner", udsProject.getOwner.getLogin)
     val name = new ObjectProperty(this, "name", udsProject.getName)
     val description = new ObjectProperty(this, "description", udsProject.getDescription)
+    val version = new ObjectProperty(this, "version", dbVersions(udsProject.getExternalDatabases))
+    val size = new ObjectProperty(this, "size",UdsRepository.calculateSize(udsProject.getId))
     // schema version
-    // taille db
+    def dbVersions(extDbs: java.util.Set[ExternalDb]): String = {
+      val vesrion = new StringBuilder()
+      var msiVersion = ""
+      var lcmsVersion = ""
+      extDbs.toList.foreach { externalDb =>
+        if (externalDb.getType() == ProlineDatabaseType.LCMS) lcmsVersion = externalDb.getDbVersion()
+        else if (externalDb.getType() == ProlineDatabaseType.MSI) msiVersion = externalDb.getDbVersion()
+      }
+      vesrion.append(msiVersion).append(" - ").append(lcmsVersion)
+      vesrion.toString
+    }
+   // taille db
+    
   }
 
   /**
