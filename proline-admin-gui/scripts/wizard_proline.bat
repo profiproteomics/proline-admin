f10aa94698fef73986ce881eddcf82d93b456f58
romdhani1987 <romdhani@live.fr>
2017-07-27 08:04:57 +0000

    [proline-Admin-Gui] change script name

--- proline-admin-gui/scripts/wizard_proline.bat
+++ proline-admin-gui/scripts/wizard_proline.bat
@@ -1,1 +1,1 @@
-java -cp "lib/*;Proline-Admin-GUI-${pom.version}.jar;config" -Dlogback.configurationFile=config/logback.xml fr.proline.admin.gui.QuickStart %*
+java -cp "lib/*;Proline-Admin-GUI-${pom.version}.jar;config" -Dlogback.configurationFile=config/logback.xml fr.proline.admin.gui.Wizard %*
