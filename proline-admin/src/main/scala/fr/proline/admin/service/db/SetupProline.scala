f162fda533cac1077365e2367e19e524cec448f3
cbruley <christophe.bruley@cea.fr>
2019-11-22 10:03:35 +0100

    - getUpdatedConfig is now private

--- proline-admin/src/main/scala/fr/proline/admin/service/db/SetupProline.scala
+++ proline-admin/src/main/scala/fr/proline/admin/service/db/SetupProline.scala
@@ -111,9 +111,9 @@ object SetupProline {
 
   // Parse config if it's not already done
   // TODO: rename to initialConfig
-  lazy val config = getUpdatedConfig()
+  lazy val config = _getUpdatedConfig()
 
-  def getUpdatedConfig() = _parseProlineSetupConfig(getConfigParams)
+  private def _getUpdatedConfig() = _parseProlineSetupConfig(getConfigParams)
 
   private def _parseProlineSetupConfig(config: Config): ProlineSetupConfig = {
 
