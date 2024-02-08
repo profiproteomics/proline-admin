4cec687ea3137bcc1bd12f3d8968bd9479ad3441
VeroDup <veronique.dupierris@cea.fr>
2024-02-08 10:54:37 +0100

    Change MSI PeaklistSoftware query (Object diff. from table ... )

--- proline-admin/src/main/scala/fr/proline/admin/service/db/migration/UpgradeMsiDbDefinitions.scala
+++ proline-admin/src/main/scala/fr/proline/admin/service/db/migration/UpgradeMsiDbDefinitions.scala
@@ -6,6 +6,8 @@ import fr.proline.core.orm.msi.{ObjectTreeSchema, Scoring}
 import fr.proline.core.orm.msi.PeaklistSoftware
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Map
 
 
 /**
@@ -22,14 +24,22 @@ class UpgradeMsiDbDefinitions(
 
     /*** Upgrade peaklist software properties ***/
     // Load existing peaklist software
-    val oldPklSofts = msiEM.createQuery("SELECT s FROM fr.proline.core.orm.msi.PeaklistSoftware s", classOf[PeaklistSoftware]).getResultList
-    val oldPklSoftByName = oldPklSofts.asScala.view.map { soft =>
-      val version = soft.getVersion
+    val oldPklSofts = msiEM.createNativeQuery("SELECT id, name, version, serialized_properties FROM peaklist_software").getResultList
+    val softList = new ArrayBuffer[PeaklistSoftware]()
+    val oldPklSoftByName = Map.empty[String, PeaklistSoftware]
+    oldPklSofts.forEach( aRow => {
+      val aSoft = new PeaklistSoftware()
+      aSoft.setId(aRow.asInstanceOf[Array[Any]].apply(0).asInstanceOf[Long])
+      aSoft.setName(aRow.asInstanceOf[Array[Any]].apply(1).toString)
+      val aVersion = aRow.asInstanceOf[Array[Any]].apply(2)
+      if(aVersion !=null)
+        aSoft.setVersion(aVersion.toString)
+      aSoft.setSerializedProperties( aRow.asInstanceOf[Array[Any]].apply(3).toString)
+      val fullSoftName = if (aVersion ==null || StringUtils.isEmpty(aVersion.toString)) aSoft.getName
+        else aSoft.getName + ' ' + aSoft.getVersion
+      oldPklSoftByName += (fullSoftName -> aSoft)
+    })
 
-      val fullSoftName = if (StringUtils.isEmpty(version)) soft.getName
-      else soft.getName + ' ' + soft.getVersion
-      fullSoftName -> soft
-    }.toMap
 
     // Index parsing rules by corresponding peaklist software
     for(nextSoft <-  fr.proline.core.orm.uds.PeaklistSoftware.SoftwareRelease.values()) {
